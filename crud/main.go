de15c45f7740f7b06e218dc267bb876f68e2dda8
Praneetha Babburi <praneethababburi@Praneethas-MacBook-Air.local>
2025-10-24 09:21:45 -0700

    changed line 140 to main branch

--- crud/main.go
+++ crud/main.go
@@ -137,7 +137,7 @@ func deleteProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("bhavani branch")
+	fmt.Println("main branch")
 	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
